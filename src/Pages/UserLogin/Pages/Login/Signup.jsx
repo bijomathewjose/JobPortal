f4102572e1428315b6c981590eae718859533ad7
Bijo Mathew Jose <bijomathewjose@gmail.com>
2023-09-21 17:42:26 +0530

    signup ui complete

--- src/Pages/UserLogin/Pages/Login/Signup.jsx
+++ src/Pages/UserLogin/Pages/Login/Signup.jsx
@@ -1,5 +1,6 @@
 import  { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import Style from './Login.module.css'
 
 const Signup = () => {
     const [invalid,setInvalid]=useState(false)
@@ -21,20 +22,28 @@ const Signup = () => {
             navigate('/login')  
         }
         else setInvalid(true)}
+    const [viewPassword,setViewPassword]=useState(false)
   return (
-    <div>
-        <h1>Welcome to JoPortal</h1>
-        <p>Please enter your details</p>
-        <form onSubmit={validate}>
-            <label htmlFor='firstname'>First Name</label><br/>
-            <input type='text' placeholder='Enter your first name' id='firstname'/><br/>
-            <label htmlFor='lastname'>Last Name</label><br/>
-            <input type='text' placeholder='Enter your last name' id='lastname'/><br/>
-            <label htmlFor='email'>Email</label><br/>
-            <input type='text' placeholder='email' id='email'/><br/>
-            <label htmlFor='password'>Password</label><br/>
-            <input type='password' placeholder='password' name='password' /><br/>
-            <button type='submit'>Signup</button>
+    <div className={Style.container}>
+       <div>
+        <h1>Welcome to Jobs</h1>
+        <p className={Style.Details}>Please enter your details</p>
+       </div>
+        <form className={Style.form} onSubmit={validate}>
+            <label className={Style.label} htmlFor='firstname'>First Name</label>
+            <input className={`${Style.input}`} type='text' placeholder='Enter your first name' id='firstname'/>
+            <label className={Style.label} htmlFor='lastname'>Last Name</label>
+            <input className={`${Style.input}`} type='text' placeholder='Enter your last name' id='lastname'/>
+            <label className={Style.label} htmlFor='email'>Email</label>
+            <input className={`${Style.input}`} type='text' placeholder='Enter your email' id='email'/>
+            <label className={Style.label} htmlFor='password'>Password</label>
+
+            <div className={Style.password}>
+              <input className={`${Style.input}`} type={viewPassword ? 'text' : 'password'}  name='password' placeholder='Enter your password'  />
+              <label  htmlFor='checkbox' className={`${Style.label} ${Style.labelViewPassword}`}>{viewPassword?'🔓':'🔒' } </label>
+              <input  className={`${Style.input}`} type='checkbox' id='checkbox' onChange={(e)=>setViewPassword(e.target.checked)}/>
+            </div>
+            <button className={Style.submit} type='submit'>Signup</button>
         </form>
         {invalid && <p style={{color:'red'}}>Invalid Credentials</p>}
         {isExists && <p style={{color:'red'}}>Email already exists</p>}
